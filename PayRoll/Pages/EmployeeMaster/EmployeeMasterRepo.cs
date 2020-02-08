57bbb350486eb4970781e3f84cefffad84417758
Debanjan Chakraborty <frontlook.inc@gmail.com>
2020-02-08 22:38:17 +0530

    update

---
+++ PayRoll/Pages/EmployeeMaster/EmployeeMasterRepo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayRoll.Pages.EmployeeMaster
+{
+    public class EmployeeMasterRepo
+    {
+        public int Id, AadharNo;
+
+        public string
+            EmployeePicture,
+            AdultRegistrationNo,
+            EmployeeCode,
+            PfNo,
+            EmployeeName,
+            FatherHusbandName,
+            Gender,
+            PermanentAddress,
+            PresentAddress,
+            Nominee,
+            MobileNo,
+            Shift,
+            Designation,
+            Department,
+            PanNo,
+            Grade,
+            Category,
+            Basic,
+            OffDay;
+
+        public DateTime DateOfJoining, DateOfBirth, LastWorkingDate;
+        public bool Active;
+
+    }
+}
